066e18615b188d95557b254b5ea7a1fb6b4b0a55
Chris <114068287+chris-fweather@users.noreply.github.com>
2024-10-22 18:31:27 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Regions from './pages/Regions';
 import News from './pages/News';
